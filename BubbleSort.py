fb68f3abfca35a79da096eb543b26f4870c3bc47
zerihunttt655674 <91532718+zerihunttt655674@users.noreply.github.com>
2021-12-16 12:59:29 +0300

    Update BubbleSort.py

--- BubbleSort.py
+++ BubbleSort.py
@@ -16,14 +16,19 @@ def countSwaps(a):
     leng = len(a)
     swaps = 0
     for i in range(leng):
+        swapped = False
         for j in range(leng-1):
             if a[j] > a[j+1]:
                 a[j],a[j+1] = a[j+1],a[j]
                 swaps+=1
+                swapped = True
+        if swapped == False:
+            break
     
     print('Array is sorted in '+ str(swaps)+' swaps.')
     print('First Element: '+str(a[0]))
     print('Last Element: '+str(a[-1]))
+    
     
 if __name__ == '__main__':
     n = int(input().strip())
