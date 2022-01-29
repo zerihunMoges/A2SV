ca20a2291aa770cf8a1320694bf08fb29e47158f
zerihunttt655674 <91532718+zerihunttt655674@users.noreply.github.com>
2022-01-29 09:01:40 +0300

    Update powx-n.py

--- powx-n.py
+++ powx-n.py
@@ -1,17 +1,21 @@
 class Solution:
     def myPow(self, x: float, n: int) -> float:
-        if n==0:
-            return 1
-        if n==1:
-            return x
-        elif n == -1:
-            return 1/x
         
-        else:
+        
+        if n>1 or n<-1:
              if n%2 == 0: 
                 result = self.myPow(x,n//2)
                 return result*result
              else:
                 result = self.myPow(x,n//2)
                 return result*result*x
-            
+        
+        elif n==1:
+            return x
+        
+        elif n == -1:
+            return 1/x
+        
+        elif n==0:
+            return 1
+        
